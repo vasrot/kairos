cc99a801aeb97e91b7fbde7887565e1a809e469d
vasrot <vasrotaru@gmail.com>
2025-08-10 12:19:42 +0200

    feat: added routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import tasksRouter from './routes/tasks.routes';
+import path from 'path';
 
 const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(morgan('dev'));
 
+// servir directorio de salida como estático
+const outputDir = process.env.OUTPUT_DIR || path.resolve('data/output');
+app.use('/output', express.static(outputDir));
+
+app.use('/tasks', tasksRouter);
 
 export default app;
